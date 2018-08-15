e77869c6e508456c19b75a42f47d1903e3f8cf7b
pawel717 <34073771+pawel717@users.noreply.github.com>
2018-08-16 00:08:03 +0200

    Update AdminAPI.java

--- VOD/src/API/AdminAPI.java
+++ VOD/src/API/AdminAPI.java
@@ -38,10 +38,11 @@ public class AdminAPI implements IAdminAPI
 	{
 		Client client = repository.getClient(clientId);
 		ArrayList<IMovie> listOfMovies = new ArrayList<IMovie>();
-		for(int movieId : listOfMovieId)
-			listOfMovies.add(repository.getMovie(movieId));
+		for(int movieeId : listOfMovieId)
+			listOfMovies.add(repository.getMovie(movieeId)); //asdad
 			
-		repository.addHire(new Hire(client, hiringPeriods, listOfMovies));	
+		repository.addHire(new Hire(client, hiringPeriods, listOfMovies));
+		repository;
 	}
 
 	@Override
